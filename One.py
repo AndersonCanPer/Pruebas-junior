eb3ecaaa44aaab51502a62dc900e373e5cf528d1
AndersonCanPer <andersoncancelada@gmail.com>
2022-06-16 22:23:25 -0500

    mi commit 1

---
+++ One.py
@@ -0,0 +1,3 @@
+numeros =[10,1,9,2,3,8,9,4,5,6,7]
+numeros.sort()
+print(numeros)
---
+++ saludo.py
@@ -0,0 +1,12 @@
+from asyncore import read
+
+
+
+print("numeros en orden descente")
+numero = [5,6,1,2]
+numero.sort(reverse=True)
+print (numero)
+
+
+
+
